84763f8939e14006c69cd8997b1c2f004220dde3
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-06-07 11:46:55 +0100

    02.06.23

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e@le7clj)vezhb!@!=#@^tiyaszz$e-#_z#3*hipdyrk_x0il@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
